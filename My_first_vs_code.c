8ce4a1f6e10e71da6b338870745faf005ba2c54e
VSchamp100 <109012162+VSchamp100@users.noreply.github.com>
2022-07-10 10:07:37 +0530

    Create My_first_vs_code.c

---
+++ My_first_vs_code.c
@@ -0,0 +1,16 @@
+- 👋 Hi, I’m @VSchamp100
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+VSchamp100/VSchamp100 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+#include<stdio.h>
+int main ()
+{
+Printf("hello");
+return 0;
+}
